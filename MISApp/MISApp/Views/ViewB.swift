2401bd54da4716aae221ac3ca4884651303c2615
emilyprivett <78056325+emilyprivett@users.noreply.github.com>
2022-11-14 19:47:10 -0600

    Update ViewB.swift

--- MISApp/MISApp/Views/ViewB.swift
+++ MISApp/MISApp/Views/ViewB.swift
@@ -10,14 +10,27 @@ import SwiftUI
 struct ViewB: View {
     var body: some View {
         ZStack {
-            Color.crimson
-            
-            Image(systemName: "desktopcomputer")
-                .foregroundColor(Color.cream)
-                .font(.system(size:100.0))
-        }.ignoresSafeArea()
+            NavigationView {
+                List {
+                    Section {
+                        Text("Meet Exec")
+                    }.foregroundColor(Color.crimson)
+                    Section {
+                        Text("General Meeting")
+                        Text("Lunch and Learn")
+                    } header: {
+                        
+                        Text("Upcoming Events")
+                    }
+                    .navigationTitle("MISSA")
+                    /*.font(.custom("Baskerville", size: 45))*/
+                    .listStyle(.insetGrouped)
+                    .navigationBarTitleDisplayMode(.large)
+                }
+            }
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.cream)
     }
-}
 
 struct ViewB_Previews: PreviewProvider {
     static var previews: some View {
